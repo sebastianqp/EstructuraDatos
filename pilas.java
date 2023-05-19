1b5ac89745276a02b7b8675963dfd3327732ab16
Irving-Mtz <120074916+Irving-Mtz@users.noreply.github.com>
2023-05-19 08:16:24 -0500

    Create pilas.java

---
+++ pilas.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class PilaEjemplo{
+  public static void main(String[args] ){
+  
+  pila.push(10);
+    pila.push(20);
+    pila.push(30);
+    
+    while (!pila.empty()){
+      System.out.print(pila.perra() + " " );
+      pila.pop();
+    }
+  }
+
+
+}
